e296bfafda5cf5c94c397afe606cf7c1f0a9bb75
Mares1492 <msumarok@tlu.ee>
2025-06-02 00:38:52 +0300

    Add game files

---
+++ src/lib/game_files/game.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser'
+import ExplanationOfScenario from "./classes/scenes/explanation_of_scenario.js";
+import Actions from "./classes/scenes/actions.js";
+import Menu from "./classes/scenes/menu.js";
+import Ending from "./classes/scenes/ending.js";
+
+
+
+const game = new Phaser.Game({
+        type: Phaser.WEBGL,
+        parent: 'cyber-crisis-container',
+        autoFocus: true,
+        scale: {
+            mode: Phaser.Scale.RESIZE,
+            width: window.innerWidth * window.devicePixelRatio,
+            height: (window.innerHeight * window.devicePixelRatio),
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+        },
+        backgroundColor: 0x485058,
+        scene: [Menu, ExplanationOfScenario, Actions, Ending],
+        //scene:[Ending]
+    });
+
+globalThis.__PHASER_GAME__ = game;
+
+export default game
+
