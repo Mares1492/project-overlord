baf680b5998cae1b7343576192eb46d76f9813c9
Mares1492 <msumarok@tlu.ee>
2025-06-03 06:10:04 +0300

    Add menu btn snippet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <div>
     
+{#snippet btn(icon)}
+    <button class="text-3xl cursor-pointer saturate-75 hover:saturate-200">
+        {icon}
+    </button>
+{/snippet}
 <div class="w-full h-dvh bg-amber-50">
     </div>
     <div class="w-full flex flex-row h-full justify-between">
