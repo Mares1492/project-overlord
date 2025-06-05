913c64edc716eaa1679912dd63aa0b0ea3599f71
Mares1492 <msumarok@tlu.ee>
2025-06-05 15:56:36 +0300

    Update schema.js

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,6 +1,8 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer,text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+	id: integer('id').primaryKey().autoIncrement(),
+	email: text('email').notNull().unique(),
+	passwordHash: text('password_hash').notNull(),
+	nickname: text('nickname').notNull(),
 });
