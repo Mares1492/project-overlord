66ff9bac2b60f8d45114307edcee77351ce0f2c3
Mares1492 <msumarok@tlu.ee>
2025-06-05 15:54:35 +0300

    Create nameGenerator.js

---
+++ src/lib/utils/nameGenerator.js
@@ -0,0 +1,20 @@
+const getRandomElement = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export const generateDarkFantasyName = () => {
+    const prefixes = [
+        "Lord", "Dread", "Arch", "High", "Wretched", "Obsidian", "Vile", "Iron", "Dark", "Crimson"
+    ];
+
+    const cores = [
+        "Malgrim", "Thorne", "Varkul", "Zaroth", "Drazhul", "Morvain", "Skarn", "Velmorr", "Grivak", "Nyther"
+    ];
+
+    const suffixes = [
+        "the Cruel", "of the Abyss", "the Eternal", "Soulrender", "the Forsaken", "Bloodreign",
+        "of Blackspire", "the Undying", "the Withered", "of the Void"
+    ];
+
+    return `${getRandomElement(prefixes)} ${getRandomElement(cores)} ${getRandomElement(suffixes)}`;
+}
