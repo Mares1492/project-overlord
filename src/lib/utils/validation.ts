cfb8098b7eab9b77f24c338aba38c93b2dc1d7a5
Mares1492 <msumarok@tlu.ee>
2025-06-05 16:07:39 +0300

    Create validation.ts

---
+++ src/lib/utils/validation.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
+});
+
+export const nicknameSchema = z.object({
+    nickname: z.string().min(3).max(64),
+})
+
+export type LoginInput = z.infer<typeof loginSchema>;
+
+export type nicknameInput = z.infer<typeof nicknameSchema>;
