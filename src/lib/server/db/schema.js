585bd46782c2a88764a4ea5c934c7f560e00473d
Mares1492 <msumarok@tlu.ee>
2025-06-06 02:37:04 +0300

    Add sessions

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,8 +1,15 @@
 import { sqliteTable, integer,text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey().autoIncrement(),
+export const users = sqliteTable('user', {
+	id: integer("id",{mode: 'number' }).primaryKey({ autoIncrement: true }),
 	email: text('email').notNull().unique(),
 	passwordHash: text('password_hash').notNull(),
 	nickname: text('nickname').notNull(),
+	uuid: text('uuid').notNull(),
+});
+
+export const sessions = sqliteTable('sessions', {
+	token: text('token').primaryKey(),
+	userId: text('user_id').notNull().references(() => users.id),
+	createdAt: integer('created_at', { mode: 'timestamp' }).default(() => Date.now())
 });
