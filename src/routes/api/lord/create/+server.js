19a8166670bf26e33ce23dbb0eeaf6eab7e035e5
Mares1492 <msumarok@tlu.ee>
2025-06-06 14:52:43 +0300

    Refactor create user route

---
+++ src/routes/api/lord/create/+server.js
@@ -0,0 +1,19 @@
+import { userSchema } from '$lib/utils/validation';
+import { json } from '@sveltejs/kit';
+import {createUser} from "$lib/server/router/user";
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+    const body = await request.json();
+    const result = userSchema.safeParse(body);
+
+    if (!result.success) {
+        return json({ error: 'Invalid input', issues: result.error.flatten() }, { status: 400 });
+    }
+
+    const { email, password } = result.data;
+
+    const newUser = await createUser(email, password);
+
+    return json({ success: true, user: { id: newUser.id, email: newUser.email } });
+}
