8ba5efef811373ac3281e82b184938cfbad297ab
Mares1492 <msumarok@tlu.ee>
2025-06-07 02:39:37 +0300

    Add keep to db schema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -13,3 +13,16 @@ export const sessions = sqliteTable('sessions', {
 	userId: text('user_id').notNull().references(() => users.id),
 	createdAt: integer('created_at', { mode: 'timestamp' })
 });
+
+export const keeps =  sqliteTable('keeps', {
+	id: integer("id",{mode: 'number' }).primaryKey({ autoIncrement: true }),
+	userId: text('user_id').notNull().references(() => users.id),
+	keepLvl: integer("keep_lvl", { mode: 'number' }).notNull().default(1),
+	barracksLvl: integer("barracks_lvl", { mode: 'number' }).notNull().default(1),
+	treasuryLvl: integer("treasury_lvl", { mode: 'number' }).notNull().default(1),
+	arsenalLvl: integer("arsenal_lvl", { mode: 'number' }).notNull().default(1),
+	academyLvl: integer("academy_lvl", { mode: 'number' }).notNull().default(0),
+	tombLvl: integer("tomb_lvl", { mode: 'number' }).notNull().default(0),
+	gold: integer("gold", { mode: 'number' }).notNull().default(500),
+	gems: integer("gems", { mode: 'number' }).notNull().default(0)
+})
