232ea33a86ba9bd71e3d62dd40a21a0abe325dca
Mares1492 <msumarok@tlu.ee>
2025-06-08 02:26:46 +0300

    Add coockie clearing functions

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -42,3 +42,18 @@ export function verifyRefreshToken(token) {
         return null;
     }
 }
+
+export function createAuthCookies(accessToken, refreshToken) {
+    return [
+        serialize('accessToken', accessToken, { ...cookieOptions, maxAge: 60 * 15 }), // 15m
+        serialize('refreshToken', refreshToken, { ...cookieOptions, maxAge: 60 * 60 * 24 * 7 }) // 7d
+    ];
+}
+
+export function clearAuthCookies() {
+    return [
+        serialize('accessToken', '', { ...cookieOptions, maxAge: 0 }),
+        serialize('refreshToken', '', { ...cookieOptions, maxAge: 0 })
+    ];
+}
+
