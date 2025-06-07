0d0a37a8ffc2e6d5d457dd91f6035eb996f1bc3f
Mares1492 <msumarok@tlu.ee>
2025-06-08 02:25:41 +0300

    Add imports

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,4 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { env } from '$env/dynamic/private';
+import cookie, { serialize } from 'cookie';
+import {getUserSessionByToken,getUserFromSession} from '$lib/server/router/user';
 
 export async function hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10); // 10 salt rounds
