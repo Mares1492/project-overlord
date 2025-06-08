73b543dae84b4deb9c0d9dc262fb6058c059dfbc
Mares1492 <msumarok@tlu.ee>
2025-06-09 01:06:23 +0300

    Add createUser

---
+++ src/lib/server/router/user.js
@@ -0,0 +1,33 @@
+import {db} from '$lib/server/db/db';
+import {sessions, users} from '$lib/server/db/schema';
+import {hashPassword, signAccessToken, signRefreshToken, verifyPassword} from '$lib/server/auth';
+import {error} from '@sveltejs/kit';
+import {randomUUID} from 'crypto';
+import {generateDarkFantasyName} from "$lib/utils/nameGenerator.js";
+import {eq} from "drizzle-orm";
+
+/**
+ * @param email {string}
+ * @param password {string}
+ */
+export const createUser = async (email, password) => {
+    // Check if user already exists
+
+    const existing = await db.select().from(users).where(eq(users.email,email)).get();
+    if (existing) {
+        throw error(409, 'Email already registered');
+    }
+
+    const passwordHash = await hashPassword(password);
+    const newUser = {
+        uuid: randomUUID(),
+        email,
+        passwordHash,
+        nickname: generateDarkFantasyName(),
+    };
+
+    await db.insert(users).values(newUser);
+
+    return newUser;
+}
+
