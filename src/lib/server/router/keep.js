188db95254c2f37a35eb0605e8177cd707c5375e
Mares1492 <msumarok@tlu.ee>
2025-06-12 14:30:00 +0300

    Update createKeep

--- src/lib/server/router/keep.js
+++ src/lib/server/router/keep.js
@@ -1,7 +1,9 @@
+import {db} from "$lib/server/db/db.js";
+import {keeps} from "$lib/server/db/schema.js";
+import {eq} from "drizzle-orm";
 
-
-export const createKeep = async () => {
-
+export const createKeep = async (userId) => {
+    await db.insert(keeps).values({userId: userId}).returning();
 }
 
 export const getKeepLvl = async (user) => {
