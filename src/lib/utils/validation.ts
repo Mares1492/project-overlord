9fe567feb953ec5f7580e97c2ddf5d0135c96178
Mares1492 <msumarok@tlu.ee>
2025-06-13 01:05:11 +0300

    Update validation.ts

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -1,14 +1,22 @@
 import { z } from 'zod';
 
-export const userSchema = z.object({
+export const userCreateSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(8)
+    password: z.string().min(8),
+    repeatPassword: z.string().min(8),
 });
+
+export const userLoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+})
 
 export const nicknameSchema = z.object({
     nickname: z.string().min(3).max(64),
 })
 
-export type userDataInput = z.infer<typeof userSchema>;
+export type createUserInput = z.infer<typeof userCreateSchema>;
+
+export type loginUserInput = z.infer<typeof userLoginSchema>;
 
 export type nicknameInput = z.infer<typeof nicknameSchema>;
