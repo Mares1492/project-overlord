01c2c5cdee39bca52a0d0f45847d9c562a9f5e49
Mares1492 <msumarok@tlu.ee>
2025-06-24 22:45:52 +0300

    Add locked item slots

--- src/routes/(authed)/[uuid]/servants/+page.svelte
+++ src/routes/(authed)/[uuid]/servants/+page.svelte
@@ -53,8 +53,15 @@
 {/snippet}
 
 {#snippet inventorySlot(i)}
-    <div class="flex flex-col cursor-pointer w-20 h-16 2xl:w-36 2xl:h-28 hover:text-black hover:bg-amber-100  items-center border-l-2 border-t-2  justify-center bg-gray-800">
-        <span class="text-3xl xl:text-4xl grayscale-90 contrast-10">~{i}</span>
+    <div class={`${i>9?i===10?'bg-gray-900':'bg-gray-800/70':'bg-gray-800'} flex flex-col border-black cursor-pointer w-20 h-16 2xl:w-36 2xl:h-28 hover:text-black hover:bg-amber-100  items-center border-l-2 border-t-2  justify-center`}>
+        <span class="flex flex-col text-xl">
+            {#if i===10}
+                <span class="text-lg">🔓</span>
+                <span class="text-yellow-500">250</span>
+            {:else if  i>10}
+                <span class="text-xl text-gray-500 grayscale-90 contrast-10">🔒</span>
+            {/if}
+        </span>
     </div>
 {/snippet}
 
