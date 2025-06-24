4ccebed6a6d975c3072794931a54225df306876d
Mares1492 <msumarok@tlu.ee>
2025-06-24 22:47:28 +0300

    Add base map

--- src/routes/(authed)/[uuid]/expeditions/+page.svelte
+++ src/routes/(authed)/[uuid]/expeditions/+page.svelte
@@ -1,7 +1,9 @@
-<p class="text-white text-bold">
-    <br><br>
-    Welcome to <strong>Expeditions</strong>
-    <br>
-    <br>
-    <i>currently under construction</i>
-</p>
+<script>
+    import BaseMap from "$lib/components/map/BaseMap.svelte";
+</script>
+
+<div class="w-full bg-amber-900/50 h-full">
+    <div class="w-1/2 flex ">
+        <BaseMap/>
+    </div>
+</div>
