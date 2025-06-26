3e1f96d9d5b4e88345f3f8a30204b434f1056f36
Mares1492 <msumarok@tlu.ee>
2025-06-27 00:43:24 +0300

    Add LocationInfoWindow to BaseMap
    Displays the LocationInfoWindow component when a location is present, passing the current mouse position and chosen location as props.

--- src/lib/components/map/BaseMap.svelte
+++ src/lib/components/map/BaseMap.svelte
@@ -204,6 +204,10 @@
 
 </script>
 
+{#if location}
+    <LocationInfoWindow position={mousePosition} location={chosenLocation}/>
+{/if}
+
 <svg class="border-2 border-amber-200/20"  viewBox="0 0 5329 4731" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="5328.06" height="4730.27" fill="#B7D8FF"/>
     <path d="M2531.2 2503.75H2404.8L2468 2397.98L2531.2 2503.75Z" fill="#5B5235" stroke="black"/>
