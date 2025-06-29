54858920155a1c4f49a67a9ca846386c6baae665
Mares1492 <msumarok@tlu.ee>
2025-06-29 13:36:31 +0300

    Add zoom controls to expeditions map page
    Introduced zoom state and handlers for zooming in and out on the expeditions map. Zoom is constrained between 1 and 5, with console logging for debugging.

--- src/routes/(authed)/[uuid]/expeditions/+page.svelte
+++ src/routes/(authed)/[uuid]/expeditions/+page.svelte
@@ -1,5 +1,24 @@
 <script>
     import BaseMap from "$lib/components/map/BaseMap.svelte";
+    let zoom = $state(1)
+
+    const handleZoomIn = () => {
+        if(zoom < 5){
+            zoom+=0.2
+            console.log(zoom)
+            return
+        }
+        zoom = 5
+
+    }
+
+    const handleZoomOut = () => {
+        if(zoom > 1){
+            zoom-=0.2
+            console.log(zoom)
+        }
+        zoom = 1
+    }
 </script>
 
 <div class="w-full overflow-auto bg-amber-900/50 h-full">
