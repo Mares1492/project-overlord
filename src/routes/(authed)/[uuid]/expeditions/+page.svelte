90fcfaf83563053bf9a916e1a826bd5dead01a5c
Mares1492 <msumarok@tlu.ee>
2025-06-29 13:36:48 +0300

    Add zoom controls to expeditions map page
    Introduces zoom in and out buttons overlayed on the map and passes the current zoom level as a prop to the BaseMap component for improved map interaction.

--- src/routes/(authed)/[uuid]/expeditions/+page.svelte
+++ src/routes/(authed)/[uuid]/expeditions/+page.svelte
@@ -22,7 +22,11 @@
 </script>
 
 <div class="w-full overflow-auto bg-amber-900/50 h-full">
-    <div class="relative">
-        <BaseMap/>
+    <div class="relative w-full h-full">
+        <div class="absolute right-5 top-3 z-1000 select-none">
+            <button onclick={handleZoomIn} class="border w-8 h-8 bg-yellow-500 cursor-pointer hover:saturate-150 active:saturate-50">+</button>
+            <button onclick={handleZoomOut} class="border w-8 h-8 bg-yellow-500 cursor-pointer hover:saturate-150 active:saturate-50">-</button>
+        </div>
+        <BaseMap scale={zoom}/>
     </div>
 </div>
