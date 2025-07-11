0f11d81d724e9c6c18f031e14c665704bdd022c7
Mares1492 <msumarok@tlu.ee>
2025-07-11 22:37:11 +0300

    Improve layout and spacing in location modal
    Added overflow-y-auto to enable scrolling in the location modal. Improved section spacing by adding mb-3.5 to Bidding and Approach headers for better visual separation.

--- src/routes/(authed)/[uuid]/expeditions/+page.svelte
+++ src/routes/(authed)/[uuid]/expeditions/+page.svelte
@@ -103,7 +103,7 @@
     </div>
 {:else if mode === modes[1] && location}
     <div class="w-full h-full flex justify-center bg-amber-900/50">
-        <div class="relative min-w-2xl h-full font-medium bg-amber-50 py-2.5 max-w-3/4 flex flex-col space-y-5 text-lg">
+        <div class="relative min-w-2xl h-full font-medium overflow-y-auto bg-amber-50 py-2.5 max-w-3/4 flex flex-col space-y-5 text-lg">
             <button onclick={closeLocation} class="absolute top-1.5 right-5 px-2 pt-0.5 text-center text-2xl cursor-pointer hover:bg-gray-500">X</button>
             <div class="flex flex-col mt-5 space-y-5">
                 <div class="text-2xl px-5">
@@ -114,7 +114,7 @@
                 <p class="px-5">{chosenLocation.description}</p>
             </div>
             <div class="border-t-2 py-3.5 flex flex-col">
-                <span class="text-xl">Bidding</span>
+                <span class="text-xl mb-3.5">Bidding</span>
                 <!-- TODO: Bidding options (Scout, Loot, Foster Ties)--> 
                 <div class="flex flex-row ">
                     <button class="w-36">Scout</button>
@@ -124,6 +124,7 @@
             </div>
             <div class="border-t-2 py-3.5 flex flex-col">
                 <span class="text-xl">Approach</span>
+                <span class="text-xl mb-3.5">Approach</span>
                 <!-- TODO: Approach  options (Stealth, Action, Situational)--> 
             </div>
         </div>
