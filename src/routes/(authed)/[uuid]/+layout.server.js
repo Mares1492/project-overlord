1f1dbac3d7849130a4a8478d3042f75b5b871630
Mares1492 <msumarok@tlu.ee>
2025-07-14 14:34:48 +0300

    Add locations data to authed layout load
    Imports locations from test data and includes it in the returned object of the authed layout server load function. This allows downstream components to access location data alongside the user UUID.

--- src/routes/(authed)/[uuid]/+layout.server.js
+++ src/routes/(authed)/[uuid]/+layout.server.js
@@ -1,6 +1,6 @@
 import {redirect} from '@sveltejs/kit';
 import {authUserSession} from "$lib/server/auth.js";
-
+import locations from "$lib/testData/locations.json";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({cookies}) {
@@ -10,5 +10,5 @@ export async function load({cookies}) {
         redirect(307, `/`);
     }
     console.log("loading authed session...");
-    return {pathUUID: user.uuid}
+    return {pathUUID: user.uuid,locations}
 }
