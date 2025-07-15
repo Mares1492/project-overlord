5a32cda9b06fa35aa38c9adc69744e0e6dfb1923
Mares1492 <msumarok@tlu.ee>
2025-07-15 19:58:36 +0300

    Update ExpSettings UI styles and add overview import
    Added import for getExpeditionOverviewText and improved button and container styling by adding rounded corners and adjusting layout classes for better appearance and usability.

--- src/lib/components/expeditions/ExpSettings.svelte
+++ src/lib/components/expeditions/ExpSettings.svelte
@@ -1,15 +1,17 @@
 <script>
     import {expeditionSettings} from '$lib/state/state.svelte';
+    import {getExpeditionOverviewText} from '$lib/handlers/expeditions.js';
+
     const {closeLocation, chosenLocation} = $props();
 
 </script>
 
 {#snippet expeditionSettingBtn (/** @type {string} */ title, handleClick, /** @type {boolean} */isChosen)}
-    <button onclick={handleClick} class:bg-yellow-200={isChosen} class="w-26 h-26 hover:bg-yellow-300 bg-slate-200 cursor-pointer">{title}</button>
+    <button onclick={handleClick} class:bg-yellow-200={isChosen} class="w-26 h-26 rounded hover:bg-yellow-300 bg-slate-200 cursor-pointer">{title}</button>
 {/snippet}
 
 <div class="w-full h-full flex justify-center bg-amber-900/50">
-    <div class="relative min-w-2xl h-full font-medium overflow-y-auto bg-amber-50 py-2.5 max-w-3/4 flex flex-col space-y-5 text-lg">
+    <div class="relative min-w-2xl min-h-full font-medium overflow-y-auto space-y-5 bg-amber-50 py-2.5 max-w-3/4 flex flex-col text-lg">
         <button onclick={closeLocation} class="absolute top-1.5 right-5 px-2 pt-0.5 text-center text-2xl cursor-pointer hover:bg-gray-500">X</button>
         <div class="flex flex-col mt-5 space-y-5">
             <div class="text-2xl px-5">
