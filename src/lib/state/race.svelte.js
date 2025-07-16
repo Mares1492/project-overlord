cf30adf6bf59bdbcba1d7de85385727c79b9a609
Mares1492 <msumarok@tlu.ee>
2025-07-16 15:25:05 +0300

    Add race asset loader for Svelte characters
    Introduces a module to dynamically import and cache face and body assets for character races. Provides an async function to retrieve assets, loading them on demand if not already cached.

---
+++ src/lib/state/race.svelte.js
@@ -0,0 +1,19 @@
+/** @type {{human:undefined|{face:string,body:string}}} */
+const raceAssets = {
+    human: undefined,
+    // Add other
+}
+/**@param {string} race */
+const importAssets = async race => {
+    raceAssets[race]= {}
+    raceAssets[race].face = await import(`$lib/assets/characters/${race.toLowerCase()}/face.webp`).then(module => module.default);
+    raceAssets[race].body = await import(`$lib/assets/characters/${race.toLowerCase()}/body.webp`).then(module => module.default);
+}
+/**@param {string} race */
+export const getRaceAssets = async race => {
+    console.log(raceAssets[race]);
+    if (!raceAssets[race]) {
+        await importAssets(race)
+    }
+    return raceAssets[race];
+}
