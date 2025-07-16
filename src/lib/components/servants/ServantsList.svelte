b9f640142c712a271d5421b41d1c1fbff3852581
Mares1492 <msumarok@tlu.ee>
2025-07-16 15:24:42 +0300

    Add ServantsList component
    Introduces ServantsList.svelte to render a list of servant slots using the CharSlot component. Supports binding servantIndex and receives servants as a prop.

---
+++ src/lib/components/servants/ServantsList.svelte
@@ -0,0 +1,9 @@
+<script>
+    import CharSlot from "$lib/components/servants/CharSlot.svelte";
+    let {servants,servantIndex=$bindable()} = $props();
+</script>
+
+
+{#each servants as servant,i(servant.name)}
+    <CharSlot {servant} {i} bind:servantIndex={servantIndex} />
+{/each}
