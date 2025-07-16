295d6d202839f61357251398b0ddc964059cf579
Mares1492 <msumarok@tlu.ee>
2025-07-16 15:24:34 +0300

    Add CharSlot component for servant selection
    Introduces CharSlot.svelte to display servant icons and handle selection. The component updates the servantIndex when clicked and visually indicates vampire servants with a hue rotation.

---
+++ src/lib/components/servants/CharSlot.svelte
@@ -0,0 +1,8 @@
+<script>
+    let {servant,i,servantIndex=$bindable()} = $props();
+
+</script>
+
+<button onclick={()=>servantIndex = i} class="relative hover:bg-amber-200 active:bg-amber-100 cursor-pointer flex flex-col w-32 h-24 2xl:w-42 2xl:h-28 items-center justify-center bg-gray-800 border-4">
+    <img class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
+</button>
