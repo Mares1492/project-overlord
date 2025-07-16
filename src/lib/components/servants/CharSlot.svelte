8bad57497d17a8d3d9e8997671aac990855ec4d0
Mares1492 <msumarok@tlu.ee>
2025-07-16 15:45:57 +0300

    Highlight selected servant slot
    Adds conditional styling to visually indicate the selected servant slot by changing background color and adding a ring. Also adjusts border thickness for consistency.

--- src/lib/components/servants/CharSlot.svelte
+++ src/lib/components/servants/CharSlot.svelte
@@ -3,6 +3,6 @@
 
 </script>
 
-<button onclick={()=>servantIndex = i} class="relative hover:bg-amber-200 active:bg-amber-100 cursor-pointer flex flex-col w-32 h-24 2xl:w-42 2xl:h-28 items-center justify-center bg-gray-800 border-4">
+<button onclick={()=>servantIndex = i} class:bg-yellow-100={servantIndex === i} class:ring-2={servantIndex === i} class="relative hover:bg-amber-200 active:bg-amber-100 cursor-pointer flex flex-col w-32 h-24 2xl:w-42 2xl:h-28 items-center justify-center bg-gray-800 border-2">
     <img class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
 </button>
