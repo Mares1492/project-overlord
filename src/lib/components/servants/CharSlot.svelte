5ccaf2d2927865bc415d2002e389e47f52500b7f
Mares1492 <msumarok@tlu.ee>
2025-07-18 00:27:13 +0300

    Add info button for expedition servants in CharSlot
    Introduces an info button for servants marked as 'isExpedition' in the CharSlot component. The button currently displays an alert as a placeholder for future info panel functionality.

--- src/lib/components/servants/CharSlot.svelte
+++ src/lib/components/servants/CharSlot.svelte
@@ -1,10 +1,15 @@
 <script>
-    let {servant,i,servantIndex=$bindable(),hasName=false} = $props();
+    let {servant,i,servantIndex=$bindable(),isExpedition=false} = $props();
+
+    const handleInfoClick = () => {
+        // Placeholder for future functionality
+        alert(`Servant ${servant.name} info btn is clicked, but panel is not implemented`);
+    }
 </script>
 
-<button onclick={()=>servantIndex = i} class:bg-yellow-100={servantIndex === i} class:ring-2={servantIndex === i} class="relative hover:bg-amber-200 active:bg-amber-100 cursor-pointer flex flex-col w-32 h-24 2xl:w-42 2xl:h-28 items-center justify-center bg-gray-800 border-2">
-    {#if hasName}
-        <span class="abosolute top-5 text-lg w-12 h-6 font-semibold bg-gray-700  text-slate-200">{servant.name}</span>
+<div class="relative w-32 h-24 2xl:w-42 2xl:h-28">
+    {#if isExpedition}
+        <button onclick={handleInfoClick} class="absolute border-slate-200 text-sm border-2 top-1 right-1 rounded-full w-6 h-6 z-10 bg-gray-800  hover:bg-amber-200 active:bg-amber-100 cursor-help">❕</button>
     {/if}
     <img class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
 </button>
