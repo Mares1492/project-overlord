1e84f802f238181d6b4079c11299d8b7266cbe5b
Mares1492 <msumarok@tlu.ee>
2025-07-18 00:27:52 +0300

    Add isExpedition prop to ServantsList
    Introduces an isExpedition prop to ServantsList.svelte and passes it to each CharSlot component. This allows CharSlot to adjust its behavior or display based on expedition context.

--- src/lib/components/servants/ServantsList.svelte
+++ src/lib/components/servants/ServantsList.svelte
@@ -1,9 +1,9 @@
 <script>
     import CharSlot from "$lib/components/servants/CharSlot.svelte";
-    let {servants,servantIndex=$bindable()} = $props();
+    let {servants,servantIndex=$bindable(),isExpedition=false} = $props();
 </script>
 
 
 {#each servants as servant,i(servant.name)}
-    <CharSlot {servant} {i} bind:servantIndex={servantIndex} />
+    <CharSlot {servant} {i} bind:servantIndex={servantIndex} isExpedition={isExpedition}/>
 {/each}
