5b871be3e2f04eb15769f4d2ec590bf725f9f008
Mares1492 <msumarok@tlu.ee>
2025-07-18 00:27:41 +0300

    Refactor CharSlot to nest servant image in button
    Moved the servant image and expedition name display inside a new button element, allowing selection of servantIndex on click. Updated styles and structure to improve interactivity and visual feedback.

--- src/lib/components/servants/CharSlot.svelte
+++ src/lib/components/servants/CharSlot.svelte
@@ -11,5 +11,12 @@
     {#if isExpedition}
         <button onclick={handleInfoClick} class="absolute border-slate-200 text-sm border-2 top-1 right-1 rounded-full w-6 h-6 z-10 bg-gray-800  hover:bg-amber-200 active:bg-amber-100 cursor-help">❕</button>
     {/if}
-    <img class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
-</button>
+    <button onclick={()=>servantIndex = i} class:bg-yellow-100={servantIndex === i}  class="relative z-0 hover:bg-amber-200 active:bg-amber-100 cursor-pointer flex flex-col  items-center justify-center w-full h-full bg-gray-800 border-2">
+        {#if isExpedition}
+            <span class:ring-2={servantIndex === i} class="absolute text-xs w-full -top-5 font-semibold border ring-black bg-gray-700  text-slate-200">{servant.name}</span>
+        {/if}
+        <img class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
+    </button>
+</div>
+
+
