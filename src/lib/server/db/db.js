7e8a08ef4c453e740a91b3d7ad80c31f14963ef8
Mares1492 <msumarok@tlu.ee>
2025-07-19 01:14:28 +0300

    Add ws

--- src/lib/server/db/db.js
+++ src/lib/server/db/db.js
@@ -1,9 +1,11 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+neonConfig.webSocketConstructor = ws;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
