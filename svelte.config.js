47fd694752755fdc1a54c7cef9d725476d1c5353
Mares1492 <msumarok@tlu.ee>
2025-07-20 00:56:50 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
 //import adapter from '@sveltejs/adapter-netlify';
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = { kit: {
-    preprocess: vitePreprocess(), 
     adapter: adapter() 
 } };
 
