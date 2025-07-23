c88a2b7c5876bd6018924adc18a4226762d15384
Mares1492 <msumarok@tlu.ee>
2025-07-23 16:59:24 +0300

    Create expeditionState.svelte.js

---
+++ src/lib/state/expeditionState.svelte.js
@@ -0,0 +1,41 @@
+export const expeditionsList = $state([]);
+let avalableExpeditionsNumber = $state(3);
+
+const expeditionStatus = {
+    IDLE: 0,
+    IN_PROGRESS: 1,
+    COMPLETED: 2,
+    FAILED: 3,
+}
+
+//exp -> expedition
+const expeditionTemplate = {
+    location: undefined,
+    endTime: undefined,
+    startTime: undefined,
+    servantId: undefined,
+    status: expeditionStatus.IDLE,
+}
+
+export const getAvailableExpeditionsNumber = () => avalableExpeditionsNumber;
+
+export const decreaseAvailableExpeditionsNumber = () => avalableExpeditionsNumber = Math.max(0, avalableExpeditionsNumber - 1);
+
+export const increaseAvailableExpeditionsNumber = () => avalableExpeditionsNumber += 1;
+
+
+
+export const addExpedition = (expeditionSettings,servantId) => {
+    const newExpedition = {...expeditionTemplate};
+    newExpedition.location = expeditionSettings.chosenLocation;
+    newExpedition.startTime = new Date();
+    let duration = expeditionSettings.scale.value * 60; // Convert minutes to second
+    newExpedition.endTime = new Date(newExpedition.startTime.getTime() + (duration>0?duration:1) * 60 * 1000);
+    newExpedition.servantId = servantId;
+    newExpedition.status = expeditionStatus.IN_PROGRESS;
+    expeditionsList.push(newExpedition);
+    decreaseAvailableExpeditionsNumber();
+    return newExpedition;
+}
+
+
