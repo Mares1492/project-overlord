ef5e5b959001e373277b5c20fe03e9b5d9d4b1f4
Mares1492 <msumarok@tlu.ee>
2025-07-23 19:52:29 +0300

    Show info text if no servants are available

--- src/lib/components/servants/ServantsList.svelte
+++ src/lib/components/servants/ServantsList.svelte
@@ -1,9 +1,16 @@
 <script>
     import CharSlot from "$lib/components/servants/CharSlot.svelte";
-    let {servants,servantIndex=$bindable(),isExpedition=false} = $props();
+    import { getServants } from "$lib/state/servants.svelte";
+    import { getAvailableExpeditionsNumber } from '$lib/state/expeditionState.svelte.js';
+
+    let {chosenServant=$bindable(),isExpedition=false} = $props();
 </script>
 
+{#if isExpedition && getAvailableExpeditionsNumber() < 1}
+    <span class="text-3xl italic font-medium text-shadow-lg text-shadow-yellow-500">No available servants</span>
+{:else}
+    {#each getServants() as servant(servant.name)}
+        <CharSlot {servant} bind:chosenServant={chosenServant} isExpedition={isExpedition}/>
+    {/each}   
+{/if}
 
-{#each servants as servant,i(servant.name)}
-    <CharSlot {servant} {i} bind:servantIndex={servantIndex} isExpedition={isExpedition}/>
-{/each}
