aed8f686f28b476699882fdb40097536e58beb51
Mares1492 <msumarok@tlu.ee>
2025-07-24 23:29:02 +0300

    Create OngoingExpeditions.svelte

---
+++ src/lib/components/expeditions/OngoingExpeditions.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {getOngoingExpeditions} from '$lib/state/expeditionState.svelte.js';
+    console.log(getOngoingExpeditions());
+</script>
+
+{#snippet expeditionSlot(expedition)}
+    <button class="flex flex-col text-sm  text-gray-800 space-y-1 w-32 min-h-24 2xl:w-42 2xl:h-28 cursor-pointer hover:text-black items-center justify-center">
+        <span class="font-bold">{expedition.location.name}</span>
+        <div class="border-t-2 border-gray-800 h-1 w-full"></div>
+        <span class="">{expedition.endTime}</span>
+    </button>
+{/snippet}
+
+<div class="flex flex-col bg-amber-400/50 border-2 p-5 justify-center space-y-7 items-center w-full h-full">
+    {#each getOngoingExpeditions() as expedition}
+        {@render expeditionSlot(expedition)}
+    {/each}
+</div>
