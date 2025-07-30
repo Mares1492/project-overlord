64679b14c13cd45545961174803052b012bf664d
Mares1492 <msumarok@tlu.ee>
2025-07-30 15:00:36 +0300

    Create enums.js

---
+++ src/lib/enums/enums.js
@@ -0,0 +1,30 @@
+export const ItemType = {
+    weapon: 0,
+    armor: 1,
+    trinket: 2,
+    consumable: 3,
+    trophy: 4
+};
+
+export const ItemRarity = {
+    common: 0,
+    uncommon: 1,
+    rare: 2,
+    epic: 3,
+    legendary: 4
+};
+
+export const LocationType = {
+  ruins: 0,
+  keep: 1,
+  river: 2,
+  neg_energy: 3,
+  meadows: 4,
+  wilds: 5,
+  swamp: 6,
+  major_settlement: 7,
+  minor_settlement: 8,
+  merc_camp: 9,
+  pos_energy: 10
+};
+
