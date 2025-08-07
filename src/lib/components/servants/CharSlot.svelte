92ccd0935bab4ad652b9e7d07e1ef235ef3753a3
Mares1492 <msumarok@tlu.ee>
2025-08-07 10:58:36 +0300

    Add expedition status

--- src/lib/components/servants/CharSlot.svelte
+++ src/lib/components/servants/CharSlot.svelte
@@ -1,5 +1,5 @@
 <script>
-    let {servant,chosenServant=$bindable(servant),isExpedition=false} = $props();
+    let {servant,chosenServant=$bindable(servant),isExpedition=false,status=1} = $props();
 
     const handleInfoClick = () => {
         // Placeholder for future functionality
@@ -17,7 +17,11 @@
                 <span class:ring-2={chosenServant.id === servant.id} class="absolute text-xs w-full -top-5 font-semibold border ring-black bg-gray-700  text-slate-200">{servant.name}</span>
             {/if}
             {#if !servant.available}
-                <span class="absolute text-xs border-y-2 w-full z-10 top-9 font-semibold  selection:none bg-white">Expedition</span>
+                {#if status > 1}
+                    <span class="absolute text-xs border-y-2 w-full z-10 top-9 font-semibold  selection:none bg-white">Waiting</span>
+                {:else}
+                    <span class="absolute text-xs border-y-2 w-full z-10 top-9 font-semibold  selection:none bg-white">Expedition</span>
+                {/if}
             {/if}
             <img class:brightness-30={!servant.available} class:grayscale-200={!servant.available} class={`absolute top-0 left-0 w-full h-full object-contain ${servant.vampire?"-hue-rotate-210":""}`} src={servant.iconPath} alt={`servant`}>
         </button>
