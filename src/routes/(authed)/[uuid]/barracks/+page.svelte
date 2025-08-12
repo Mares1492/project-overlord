c6cdef715f3c83fbbcb71ffbc6469235a0308e80
Mares1492 <msumarok@tlu.ee>
2025-08-13 00:40:05 +0300

    Minor changes

--- src/routes/(authed)/[uuid]/barracks/+page.svelte
+++ src/routes/(authed)/[uuid]/barracks/+page.svelte
@@ -10,8 +10,8 @@
         {name:"War Room",lvl:0,updateBase:1000,icon:`🐉️`},
         {name:"Night Training Grounds",lvl:0,updateBase:250,icon:`🐱‍👤`},
         {name:"Library",lvl:0,updateBase:2500,icon:`📚`},
-        {name:"Blacksmiths's Annex",lvl:0,updateBase:1000,icon:`🛡️`},
-        {name:"Warcaster’s Hall",lvl:0,updateBase:10000,icon:`🧙🏻`},
+        {name:"Blacksmith's Annex",lvl:0,updateBase:1000,icon:`🛡️`},
+        {name:"Warcaster`s Hall",lvl:0,updateBase:10000,icon:`🧙🏻`},
 
     ]
 </script>
@@ -20,7 +20,7 @@
     {#each updates as update(update.name)}
         <div class="flex flex-col items-center">
             <span class="text-white text-lg">{update.name}</span>
-            <button class="hover:bg-amber-100 cursor-pointer border border-black hover:text-black hover:font-bold text-white flex flex-col w-32 h-24 xl:w-42 xl:h-28 justify-around bg-gray-800 border-2">
+            <button class="hover:bg-amber-100 cursor-pointer border-black hover:text-black hover:font-bold text-white flex flex-col w-32 h-24 xl:w-42 xl:h-28 justify-around bg-gray-800 border-2">
                 <span class="xl:text-lg text-base grayscale-50 saturate-125">Lvl: {update.lvl}</span>
                 <span class="text-3xl xl:text-4xl grayscale-50 saturate-125 text-center">{update.icon}</span>
                 <span class="text-lg xl:text-xl text-yellow-500">{update.updateBase*(update.lvl+1)}</span>
