d8f9fb0b44cd70682df9d0fef702378c1e07ba04
Mares1492 <msumarok@tlu.ee>
2025-08-16 01:46:48 +0300

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'drizzle-kit';
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+const connectionString =
+  process.env.DATABASE_URL_MAIN && process.env.USE_PROD === "true"
+    ? process.env.DATABASE_URL_MAIN // prod Neon branch
+    : process.env.DATABASE_URL;    // dev Neon branch
+
 export default defineConfig({
 	out: './drizzle',
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: { url: connectionString },
 	verbose: true,
 	strict: true
 });
