3dba19db62cf516db822a29577b8b3345068340d
Mares1492 <msumarok@tlu.ee>
2025-08-19 00:53:45 +0300

    Update deleteKeep

--- src/lib/server/router/keep.js
+++ src/lib/server/router/keep.js
@@ -7,12 +7,18 @@ export const createKeep = async (userId) => {
 	return db.insert(keeps).values({ userId }).returning();
 };
 
-// Delete a keep associated with a user
-export const deleteKeep = async (userId) => {
-	return db.delete(keeps).where(eq(keeps.userId, userId)).returning();
+export const deleteKeep = async (tx,userId) => {
+	const [keepToDelete] = await db.select().from(keeps).where(eq(keeps.userId, userId));
+	const [barracksToDelete] = await db.select().from(barracks).where(eq(barracks.keepId, keepToDelete.id));
+	await tx.delete(barracksExtensionBuildings).where(eq(barracksExtensionBuildings.barracksId, barracksToDelete.id));
+	await tx.delete(arsenals).where(eq(arsenals.keepId, keepToDelete.id));
+	await tx.delete(academies).where(eq(academies.keepId, keepToDelete.id));
+	await tx.delete(tombs).where(eq(tombs.keepId, keepToDelete.id));
+	await tx.delete(treasuries).where(eq(treasuries.keepId, keepToDelete.id));
+	await tx.delete(barracks).where(eq(barracks.keepId, keepToDelete.id));
+	return tx.delete(keeps).where(eq(keeps.userId, userId)).returning();
 };
 
-// Fetch the keep of a user
 export const getKeep = async (userId) => {
 	return db.query.keeps.findFirst({
 		where: eq(keeps.userId, userId)
