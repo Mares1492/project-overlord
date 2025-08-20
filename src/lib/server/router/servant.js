56e27f472785f189e3e9b80c0de0473e825c839f
Mares1492 <msumarok@tlu.ee>
2025-08-21 00:45:36 +0300

    Add servant deletion

--- src/lib/server/router/servant.js
+++ src/lib/server/router/servant.js
@@ -1,6 +1,7 @@
 import { db } from "$lib/server/db/db.js";
 import { getRandomServantName } from '$lib/server/handlers/generators.js';
 import {servants,servantAttributes} from "$lib/server/db/schema";
+import { eq } from "drizzle-orm";
 
 const getRandomAttributeValue = () => {
     return Math.floor(Math.random() * 5) + 1;
@@ -44,10 +45,12 @@ const createServantAttributes = async (tx, servantId) => {
     })
 }
 
+// can be called as user creation transaction to create initial servant
+// or as a separate transaction or db call to create a new servant for an existing user
 /** 
  * @param {number} userId 
  */
-export const createServant = async (userId, servantData,tx=db) => {
+export const createServant = async (userId, servantData, tx=db) => {
 
     const templateServant = {
         userId: userId,
@@ -61,8 +64,27 @@ export const createServant = async (userId, servantData,tx=db) => {
         .returning();
         
     createServantAttributes(tx, newServant.id);
-    
-
 
     return newServant;
+}
+
+export const deleteServant = async (tx, servantId) => {
+    await tx.delete(servantAttributes).where(eq(servantAttributes.servantId, servantId));
+    return tx.delete(servants).where(eq(servants.id, servantId)).returning();
+}
+
+export const deleteServantsByUserId = async (tx, userId) => {
+    const servantsToDelete = await tx
+        .select()
+        .from(servants)
+        .where(eq(servants.userId, userId));
+
+    if (servantsToDelete.length === 0) {
+        throw new Error(`No servants found for user with ID ${userId}`);
+    }
+
+    const servantIds = servantsToDelete.map(servant => servant.id);
+    for (const servantId of servantIds) {
+        await deleteServant(tx, servantId);
+    }
 }
