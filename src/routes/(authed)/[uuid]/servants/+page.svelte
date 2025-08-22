c5b46e85bf46e0ed99edce91118c36e5cd05ae0c
Mares1492 <msumarok@tlu.ee>
2025-08-23 01:35:09 +0300

    MAke attributes display element wider

--- src/routes/(authed)/[uuid]/servants/+page.svelte
+++ src/routes/(authed)/[uuid]/servants/+page.svelte
@@ -101,7 +101,7 @@
     </div>
     <div class="w-1/2 h-full flex flex-row">
         {#if chosenServant}
-            <ul class="h-full flex flex-col justify-center space-y-2 border-4 border-amber-950 bg-orange-950 text-white w-32 px-5">
+            <ul class="h-full flex flex-col justify-center space-y-2 border-4 border-amber-950 bg-orange-950 text-white w-38 px-5">
                 {#each chosenServant.attributes as attribute(attribute.name)}
                     <li class="flex flex-row space-x-2 justify-between items-end">
                         <span class="text-xl 2xl:text-2xl">{attribute.shortName}:</span><span class="font-semibold text-2xl 2xl:text-3xl">{attribute.value}</span>
