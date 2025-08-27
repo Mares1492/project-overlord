30ae8851efec70196d1c48f032c40b9afe3504d7
Mares1492 <msumarok@tlu.ee>
2025-08-27 16:58:33 +0300

    Add startExpedition action

--- src/routes/(authed)/[uuid]/expeditions/+page.server.js
+++ src/routes/(authed)/[uuid]/expeditions/+page.server.js
@@ -1,4 +1,5 @@
 import {getAllLocations} from '$lib/server/router/locations'
+import {createExpedition} from '$lib/server/router/expeditions'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
@@ -11,14 +12,26 @@ export async function load() {
 
 /** @satisfies {import('./$types').Actions} */
 export const actions = {
-    startExpedition: async ({ request }) => {
+    startExpedition: async ({ request,params }) => {
         const data = await request.formData();
+        const userUUID = params.uuid;
         const servantUUID = data.get('servantUUID');
+        const locationId = data.get('locationId');
         const expeditionSettings = {
-            task: data.get('task'),
-            scale: data.get('scale'),
-            approach: data.get('approach')
+            taskId: data.get('taskId'),
+            scaleId: data.get('scaleId'),
+            approachId: data.get('approachId')
         };
+        const overviewText = data.get('overviewText')
+        const newExpedition = await createExpedition(
+            userUUID,
+            locationId,
+            servantUUID,
+            expeditionSettings,
+            overviewText
+        )
+        console.log(newExpedition)
+        return {newExpedition}
     },
     completeExpedition: async ({ request }) => {
         const data = await request.formData();
