d317a2cdc33a9e80268e1fee16d467030498a31c
Mares1492 <msumarok@tlu.ee>
2025-08-27 15:30:43 +0300

    Add getAllLocations

--- src/lib/server/router/locations.js
+++ src/lib/server/router/locations.js
@@ -1,3 +1,23 @@
+import { db } from "../db/db"
+import { locationImportanceTypes, locations } from "../db/schema"
+import { eq } from "drizzle-orm";
+
 export const getLocation = () => {
     
+}
+export const getAllLocations = async () => {
+    return db
+    .select({
+        id: locations.id,
+        name: locations.name,
+        importance: locationImportanceTypes.value,
+        description: locations.description,
+        coords: {x:locations.coordsX,y:locations.coordsY},
+        color: locations.color_hex,
+        type: locations.locationTypeId,
+        is_base: locations.isBase
+    })
+    .from(locations)
+    .innerJoin(locationImportanceTypes, eq(locationImportanceTypes.id, locations.importanceTypeId))
+
 }
