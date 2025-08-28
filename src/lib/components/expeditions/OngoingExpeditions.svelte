a992eac0db0d5b38e60c009823d8b856cf52526b
Mares1492 <msumarok@tlu.ee>
2025-08-29 01:36:09 +0300

    Refactor time count display system

--- src/lib/components/expeditions/OngoingExpeditions.svelte
+++ src/lib/components/expeditions/OngoingExpeditions.svelte
@@ -1,91 +1,97 @@
 <script>
-    import {completeExpedition,archiveExpedition } from '$lib/state/expeditionState.svelte.js';
     import {onMount} from 'svelte';
    	import { slide,scale } from 'svelte/transition';
     import {ExpeditionStatus} from '$lib/enums/enums.js';
     import { goto } from '$app/navigation';
+    import { enhance } from '$app/forms';
 
     const {pathUUID,ongoingExpeditions} = $props()
 
-    let expeditions = $state([]);
-    $inspect(expeditions)
+    
+    /**
+        * @typedef {Object.<string, TimeObject>} expeditionsTimeCounts
+         * @typedef {Object} TimeObject
+         * @property {number} seconds
+         * @property {number} minutes
+         * @property {number} hours
+     */
+    let expeditionsTimeCounts = $state({});
+    let chosenExpeditionUUID = $state("");
     let pageState = $state({
         loading: true,
         isBlocking: false,
         error: false
     });
 
-    onMount(() => {
-        const interval = setInterval( () => {
-            pageState.loading = false;
-            if (!ongoingExpeditions) {
-                return clearInterval(interval)
-            }
-            expeditions = ongoingExpeditions.map( exp => {
-                if (exp.status === ExpeditionStatus.COMPLETED) {
-                    return {
-                        uuid: exp.uuid,
-                        name: exp.location.name,
-                        servant: exp.servant,
-                        status: exp.status,
-                        task: exp.task,
-                        approach: exp.approach,
-                        scale: exp.scale,
-                    }
-                }
+    const updateTimeCounts = () => {
+        let isNoActiveExpeditions = true
+        console.log(ongoingExpeditions)
+        if (!ongoingExpeditions) {
+            return
+        }
+        ongoingExpeditions.forEach( exp => {
+                if (exp.status !== ExpeditionStatus.IN_PROGRESS) return 
                 let msLeft = exp.endTime - Date.now();
                 if (msLeft <= 0) {
-                    if (exp.status === ExpeditionStatus.IN_PROGRESS) {
-                        completeExpedition(exp.id);
+                    expeditionsTimeCounts[exp.uuid] = {
+                        hours:0,
+                        minutes:0,
+                        seconds:0
                     }
-                    return {
-                        uuid: exp.uuid,
-                        name: exp.location.name,
-                        servant: exp.servant,
-                        status: ExpeditionStatus.COMPLETED,
-                        task: exp.task,
-                        approach: exp.approach,
-                        scale: exp.scale,
-                    };
+                    return
                 }
+                isNoActiveExpeditions = false
                 let totalSeconds = Math.floor(msLeft / 1000);
 
                 let hours = Math.floor(totalSeconds / 3600);
                 let minutes = Math.floor((totalSeconds % 3600) / 60);
                 let seconds = totalSeconds % 60;
                 
-            
-                return {
-                    uuid: exp.uuid,
-                    name: exp.location.name,
-                    servant: exp.servant,
-                    status: exp.status,
+                expeditionsTimeCounts[exp.uuid] = {
                     hours,
                     minutes,
-                    seconds,
-                    task: exp.task,
-                    approach: exp.approach,
-                    scale: exp.scale,
-                };
+                    seconds
+                }
                 
+                isNoActiveExpeditions = false
             });
+            return isNoActiveExpeditions
+        }
+
+    onMount(() => {
+        updateTimeCounts()
+        const interval = setInterval( () => {
+            pageState.loading = false;
+            if (!ongoingExpeditions || !ongoingExpeditions.length) {
+                return clearInterval(interval)
+            }
+            let isNoActiveExpeditions = updateTimeCounts()
+            if (isNoActiveExpeditions) {
+                return clearInterval(interval)
+            }
         }, 1000);
         return () => clearInterval(interval);
     });
-
-    const handleCompleteClick = (expId) => {
-        if(!archiveExpedition(expId)){
-            pageState.error = true
-        }
-    }
 
     const handleExpeditionExpandBtnClick = (expeditionId) => {
         //TODO: use action with server check instead
         goto(`/${pathUUID}/expeditions/${expeditionId}`)
     }
 
+    const setFocusedExpeditionUUID = (expeditionUUID) => {
+        chosenExpeditionUUID=expeditionUUID;
+    }
 
 </script>
+
+<form id="completeExpedition" method="POST" action="?/completeExpedition" use:enhance>
+    <input type="hidden" name="expeditionUUID" value={chosenExpeditionUUID}>
+</form>
+
+<form id="archiveExpedition" method="POST" action="?/archiveExpedition" use:enhance>
+    <input type="hidden" name="expeditionUUID" value={chosenExpeditionUUID}>
+</form>
+
 
 {#snippet expeditionSlot(expedition)}
     <div transition:slide|global={{delay:100}} class="relative flex flex-col text-sm w-full text-gray-800 space-y-1 hover:text-black items-center justify-center">
@@ -103,19 +109,21 @@
         <span class="text-gray-600">Task: <i>{expedition.task}</i></span>
         <span class="text-gray-600">Approach: <i>{expedition.approach}</i></span>
         <span class="text-gray-600">Scale: <i>{expedition.scale}</i></span>
-        {#if expedition.status === ExpeditionStatus.COMPLETED}
-            <button onclick={()=>handleCompleteClick(expedition.id)} class="bg-green-500 hover:bg-green-400 active:bg-green-300 px-2 py-1 rounded cursor-pointer text-white font-semibold">Complete</button>
-        {:else}
+        {#if  expedition.status === ExpeditionStatus.IN_PROGRESS}
+            <button form="completeExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class="mt-3 bg-green-500 hover:bg-green-400 active:bg-green-300 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Complete</button>
+        {:else if expedition.status === ExpeditionStatus.COMPLETED}
+            <button form="archiveExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class="mt-3 bg-orange-600 hover:bg-amber-600 active:bg-yellow-600 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Archive</button>
+        {:else if expeditionsTimeCounts[expedition.uuid]}
             <div class="flex flex-row items-center justify-center font-semibold">
-                <span class="border w-12 px-2">{expedition.hours}h</span>:
-                <span class="border  w-12 px-2">{expedition.minutes}m</span>:
-                <span class="border  w-12 px-2">{expedition.seconds}s</span>
+                <span class="border w-12 px-2">{expeditionsTimeCounts[expedition.uuid].hours}h</span>:
+                <span class="border  w-12 px-2">{expeditionsTimeCounts[expedition.uuid].minutes}m</span>:
+                <span class="border  w-12 px-2">{expeditionsTimeCounts[expedition.uuid].seconds}s</span>
             </div>
         {/if}
     </div>
 {/snippet}
 
-{#if expeditions.length === 0}
+{#if ongoingExpeditions.length === 0}
     {#if pageState.loading}
         <div class="flex w-full flex-col items-center justify-center ">
             <span class="text-2xl w-full font-bold">Loading ongoing expeditions...</span>
@@ -128,7 +136,7 @@
     {/if}
 {:else}
     <div class="flex flex-col bg-amber-400/70 border-2 p-5 justify-center space-y-7 items-center w-full">
-        {#each expeditions as expedition}
+        {#each ongoingExpeditions as expedition}
             {@render expeditionSlot(expedition)}
         {/each}
     </div>
