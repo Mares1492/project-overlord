383e6891a84073221ab1c4a001a15828419f2ca5
Mares1492 <msumarok@tlu.ee>
2025-08-29 21:22:48 +0300

    Use servants from db

--- src/lib/components/servants/ServantsList.svelte
+++ src/lib/components/servants/ServantsList.svelte
@@ -1,19 +1,19 @@
 <script>
     import CharSlot from "$lib/components/servants/CharSlot.svelte";
-    import { getServants } from "$lib/state/servants.svelte";
-    import { getAvailableExpeditionsNumber } from '$lib/state/expeditionState.svelte.js';
     import {ServantStatus} from '$lib/enums/enums'
 
-    let {chosenServant=$bindable(),isExpeditionSettings=false} = $props();
+    let {servants, chosenServant=$bindable(),isExpeditionSettings=false} = $props();
 </script>
 
-{#if isExpeditionSettings && getAvailableExpeditionsNumber() < 1 || getServants().length === 0}
-    <span class="text-3xl italic font-medium text-shadow-sm text-shadow-yellow-500">No available servants</span>
-{:else}
-    {#each getServants() as servant(servant.uuid)}
-        {#if isExpeditionSettings && servant.status.id === ServantStatus.IDLE}
-            <CharSlot {servant} bind:chosenServant={chosenServant} isExpeditionSettings={isExpeditionSettings}/>  
-        {/if}
-    {/each}   
+{#if servants}
+    {#if servants.length === 0}
+        <span class="text-3xl italic font-medium text-shadow-sm text-shadow-yellow-500">No available servants</span>
+    {:else}
+        {#each servants as servant(servant.uuid)}
+            {#if !isExpeditionSettings || servant.status.id === ServantStatus.IDLE}
+                <CharSlot {servant} bind:chosenServant={chosenServant} isExpeditionSettings={isExpeditionSettings}/>  
+            {/if}
+        {/each}   
+    {/if}
 {/if}
 
