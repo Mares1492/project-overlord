afc7ad182a7d1b7f435c6b521dc70e100b4cf3c9
Mares1492 <msumarok@tlu.ee>
2025-08-29 21:25:52 +0300

    Send availableServants number

--- src/routes/(authed)/[uuid]/+layout.server.js
+++ src/routes/(authed)/[uuid]/+layout.server.js
@@ -3,15 +3,15 @@ import {authUserSession} from "$lib/server/auth.js";
 import {getServantsByUserUUID} from '$lib/server/router/servants.js';    
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load({cookies}) {
+export async function load({cookies,params}) {
     const {user} = await authUserSession(cookies)
     if (!user) {
-        redirect(307, `/`);
+        redirect(308, `/`);
     }
-    const servants = await getServantsByUserUUID(user.uuid)
+    const {servants,availableServants} = await getServantsByUserUUID(user.uuid)
     if (!servants) {
         return {pathUUID: user.uuid, error: true, message: "No servants found for this user"};
     }
     console.log("loading authed session...");
-    return {pathUUID: user.uuid, servants}
+    return {pathUUID: user.uuid, servants, availableServants}
 }
