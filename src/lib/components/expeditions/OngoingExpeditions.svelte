4083385c80ce39a78faa98c6dc1833b3abb54c66
Mares1492 <msumarok@tlu.ee>
2025-08-29 04:59:06 +0300

    Fix display issues

--- src/lib/components/expeditions/OngoingExpeditions.svelte
+++ src/lib/components/expeditions/OngoingExpeditions.svelte
@@ -23,54 +23,49 @@
         error: false
     });
 
+
     const updateTimeCounts = () => {
-        let isNoActiveExpeditions = true
-        console.log(ongoingExpeditions)
         if (!ongoingExpeditions) {
-            return
+            return 
         }
         ongoingExpeditions.forEach( exp => {
-                if (exp.status !== ExpeditionStatus.IN_PROGRESS) return 
-                let msLeft = exp.endTime - Date.now();
-                if (msLeft <= 0) {
-                    expeditionsTimeCounts[exp.uuid] = {
-                        hours:0,
-                        minutes:0,
-                        seconds:0
-                    }
-                    return
-                }
-                isNoActiveExpeditions = false
-                let totalSeconds = Math.floor(msLeft / 1000);
-
-                let hours = Math.floor(totalSeconds / 3600);
-                let minutes = Math.floor((totalSeconds % 3600) / 60);
-                let seconds = totalSeconds % 60;
-                
+            let msLeft = exp.endTime.getTime() - Date.now();
+            if (msLeft <= 0) {
                 expeditionsTimeCounts[exp.uuid] = {
-                    hours,
-                    minutes,
-                    seconds
+                    hours:0,
+                    minutes:0,
+                    seconds:0,
+                    msLeft
                 }
-                
-                isNoActiveExpeditions = false
+                return
+            }
+            let totalSeconds = Math.floor(msLeft / 1000);
+
+            let hours = Math.floor(totalSeconds / 3600);
+            let minutes = Math.floor((totalSeconds % 3600) / 60);
+            let seconds = totalSeconds % 60;
+            
+            expeditionsTimeCounts[exp.uuid] = {
+                hours,
+                minutes,
+                seconds,
+                msLeft
+            }
+            
             });
-            return isNoActiveExpeditions
-        }
+    }
+    
+    const setTimeCountsTimeout = () => {
+        const interval = setInterval( () => {
+            pageState.loading = false;
+            updateTimeCounts()
+        }, 1000);
+        return () => clearInterval(interval);
+    }
 
     onMount(() => {
         updateTimeCounts()
-        const interval = setInterval( () => {
-            pageState.loading = false;
-            if (!ongoingExpeditions || !ongoingExpeditions.length) {
-                return clearInterval(interval)
-            }
-            let isNoActiveExpeditions = updateTimeCounts()
-            if (isNoActiveExpeditions) {
-                return clearInterval(interval)
-            }
-        }, 1000);
-        return () => clearInterval(interval);
+        setTimeCountsTimeout()
     });
 
     const handleExpeditionExpandBtnClick = (expeditionId) => {
@@ -94,32 +89,39 @@
 
 
 {#snippet expeditionSlot(expedition)}
-    <div transition:slide|global={{delay:100}} class="relative flex flex-col text-sm w-full text-gray-800 space-y-1 hover:text-black items-center justify-center">
-        <button 
-            class="absolute -top-1.5 right-1 text-gray-800 cursor-pointer text-shadow-2xs hover:text-shadow-md text-shadow-yellow-600 text-xl hover:text-2xl transition-all hover:translate-x-0.5 hover:-translate-y-0.5"
-            onclick={()=>handleExpeditionExpandBtnClick(expedition.uuid)}
-            in:scale|global={{delay:500}}
-            out:scale|global={{duration:100}}
-        > 
-            📜
-        </button>
-        <span class="font-bold">{expedition.name}</span>
-        <div class="border-t-2 border-gray-800 h-1 w-full"></div>
+    <div in:slide={{delay:300}} out:slide class="relative flex flex-col text-sm w-full h-40 text-gray-800 space-y-1 items-center justify-center">
+        <div class="flex flex-row items-center justify-center relative w-full">
+            <span class="font-bold self-center">{expedition.location.name}</span>
+            <button 
+                class="absolute right-1 text-gray-800 cursor-pointer text-shadow-2xs hover:text-shadow-md text-shadow-yellow-600 text-xl hover:text-2xl transition-all hover:translate-x-0.5 hover:-translate-y-0.5"
+                onclick={()=>handleExpeditionExpandBtnClick(expedition.uuid)}
+                in:scale|global={{delay:500}}
+                out:scale|global={{duration:100}}
+            > 
+                📜
+            </button>
+        </div>
+        
+        <div class="border-t-2 border-gray-800 h-1 w-full mb-2"></div>
         <span class="text-gray-800">{expedition.servant.name}</span>
         <span class="text-gray-600">Task: <i>{expedition.task}</i></span>
         <span class="text-gray-600">Approach: <i>{expedition.approach}</i></span>
         <span class="text-gray-600">Scale: <i>{expedition.scale}</i></span>
-        {#if  expedition.status === ExpeditionStatus.IN_PROGRESS}
-            <button form="completeExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class="mt-3 bg-green-500 hover:bg-green-400 active:bg-green-300 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Complete</button>
-        {:else if expedition.status === ExpeditionStatus.COMPLETED}
-            <button form="archiveExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class="mt-3 bg-orange-600 hover:bg-amber-600 active:bg-yellow-600 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Archive</button>
-        {:else if expeditionsTimeCounts[expedition.uuid]}
-            <div class="flex flex-row items-center justify-center font-semibold">
-                <span class="border w-12 px-2">{expeditionsTimeCounts[expedition.uuid].hours}h</span>:
-                <span class="border  w-12 px-2">{expeditionsTimeCounts[expedition.uuid].minutes}m</span>:
-                <span class="border  w-12 px-2">{expeditionsTimeCounts[expedition.uuid].seconds}s</span>
-            </div>
-        {/if}
+        <div class="mt-2 h-14">
+            {#if expeditionsTimeCounts[expedition.uuid]}
+                {#if expedition.status === ExpeditionStatus.IN_PROGRESS && expeditionsTimeCounts[expedition.uuid].msLeft < 1 }
+                    <button form="completeExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class=" bg-green-500 hover:bg-green-400 active:bg-green-300 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Complete</button>
+                {:else if expedition.status === ExpeditionStatus.COMPLETED}
+                    <button form="archiveExpedition" onfocusin={()=>setFocusedExpeditionUUID(expedition.uuid)} onmouseenter={()=>setFocusedExpeditionUUID(expedition.uuid)} type="submit" class=" bg-orange-600 hover:bg-amber-600 active:bg-yellow-600 px-2 py-1 rounded cursor-pointer text-slate-100 font-semibold">Archive</button>
+                {:else}
+                    <div class="flex flex-row items-center justify-center font-semibold ">
+                        <span class="border w-12 px-2">{expeditionsTimeCounts[expedition.uuid].hours}h</span>:
+                        <span class="border w-12 px-2">{expeditionsTimeCounts[expedition.uuid].minutes}m</span>:
+                        <span class="border w-12 px-2">{expeditionsTimeCounts[expedition.uuid].seconds}s</span>
+                    </div>
+                {/if}
+            {/if}
+        </div>
     </div>
 {/snippet}
 
@@ -135,8 +137,8 @@
         </div>
     {/if}
 {:else}
-    <div class="flex flex-col bg-amber-400/70 border-2 p-5 justify-center space-y-7 items-center w-full">
-        {#each ongoingExpeditions as expedition}
+    <div transition:slide|global={{duration:300}} class="flex flex-col max-h-264 overflow-y-auto mini-scrollbar bg-amber-400/70 border-2 p-5 justify-start space-y-7 items-center">
+        {#each ongoingExpeditions as expedition(expedition.uuid)}
             {@render expeditionSlot(expedition)}
         {/each}
     </div>
@@ -147,3 +149,25 @@
     </div>
 {/if}
 
+<style>
+    .mini-scrollbar {
+        overflow-y: auto;
+    }
+
+    /* scrollbar track (background) */
+    .mini-scrollbar::-webkit-scrollbar {
+        width: 6px;   /* makes it thin */
+        height: 6px;  /* for horizontal scroll */
+    }
+
+    /* scrollbar thumb (the draggable part) */
+    .mini-scrollbar::-webkit-scrollbar-thumb {
+        background-color: rgba(100, 100, 100, 0.5);
+        border-radius: 10px;
+    }
+
+    /* optional: hover effect */
+    .mini-scrollbar::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(100, 100, 100, 0.8);
+    }
+</style>
