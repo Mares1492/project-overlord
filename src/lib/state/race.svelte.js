2049a509879eed4bb78ef79148b3b0516699b6e2
Mares1492 <msumarok@tlu.ee>
2025-08-29 21:24:41 +0300

    Adjust to use as assets state module

--- src/lib/state/race.svelte.js
+++ src/lib/state/race.svelte.js
@@ -1,18 +1,49 @@
-/** @type {{human:undefined|{face:string,body:string}}} */
-const raceAssets = {
-    human: undefined,
-    // Add other
+import {invertedRaceTypes} from '$lib/enums/enums'
+
+/**
+ * @typedef {Object} AssetObject
+ * @property {string} face
+ * @property {string} body
+ * @typedef {Object.<string, AssetObject>} RaceAssets
+ */
+
+const setRaceTypes = () => {
+    /** @type {RaceAssets} */
+    let raceTypes = {}
+    for (let race of Object.values(invertedRaceTypes)){
+        raceTypes[race.toLowerCase()] = {
+            face:"",
+            body:"",
+        }
+    }
+    return raceTypes
 }
+
+/**@param {string} race */
+const getRaceString = (race) => race.replace(' ','_').toLocaleLowerCase()
+
+/**@type {RaceAssets} */
+const raceAssets = $state(setRaceTypes())
+
 /**@param {string} race */
 const importAssets = async race => {
-    raceAssets[race]= {}
     raceAssets[race].face = await import(`$lib/assets/characters/${race.toLowerCase()}/face.webp`).then(module => module.default);
     raceAssets[race].body = await import(`$lib/assets/characters/${race.toLowerCase()}/body.webp`).then(module => module.default);
+    return raceAssets[race]
 }
-/**@param {string} race */
-export const getRaceAssets = async race => {
-    if (!raceAssets[race]) {
-        await importAssets(race)
+
+export const loadRaceAssets = async servants => {
+    for (let servant of servants) {
+        let raceString = getRaceString(servant.race)
+        await importAssets(raceString)
     }
-    return raceAssets[race];
 }
+
+/**@param {string} race */
+export const getRaceAssets = (race) => raceAssets[getRaceString(race)];
+
+/**@param {string} race */
+export const getRaceFace = (race) => getRaceAssets(race).face
+
+/**@param {string} race */
+export const getRaceBody = (race) => getRaceAssets(race).body
