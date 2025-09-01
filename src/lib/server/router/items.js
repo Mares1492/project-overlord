294639613f46c49f538d6b300f6135111042131e
Mares1492 <msumarok@tlu.ee>
2025-09-01 20:27:41 +0300

    Add itemDropChances

--- src/lib/server/router/items.js
+++ src/lib/server/router/items.js
@@ -1,3 +1,11 @@
+const itemDropChances = {
+    [ItemRarity.common]: 0.5,
+    [ItemRarity.uncommon]: 0.3,
+    [ItemRarity.rare]: 0.1,
+    [ItemRarity.epic]: 0.06,
+    [ItemRarity.mythic]: 0.03,
+    [ItemRarity.legendary]: 0.01,
+}
 export const getRandomItemOfGivenRarity = async (rarityTypeId) => {
     // random item of given rarity, in any category
     const [randomItem] = await db
