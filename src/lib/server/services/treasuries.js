fac11a38453c95b361de70de622f01be7f55063d
Mares1492 <msumarok@tlu.ee>
2025-09-10 23:56:41 +0300

    Create addToTreasury

--- src/lib/server/services/treasuries.js
+++ src/lib/server/services/treasuries.js
@@ -1,5 +1,5 @@
 import { db } from "$lib/server/db/db.js";
-import { eq } from "drizzle-orm";
+import { eq,sql } from "drizzle-orm";
 import { treasuries,keeps } from "../db/schema";
 
 export const getTreasuryData = async (userId) => {
@@ -12,4 +12,13 @@ export const getTreasuryData = async (userId) => {
         .innerJoin(keeps,eq(keeps.userId,userId))
         .where(eq(treasuries.keepId,keeps.id)).limit(1)
     return treasuryData
+}
+
+export const addToTreasury = async (treasuryId,goldToAdd=0,gemsToAdd=0) => {
+    await db.update(treasuries)
+    .set({
+        gold: sql`${treasuries.gold} + ${goldToAdd}`,
+        gems: sql`${treasuries.gems} + ${gemsToAdd}`,
+    })
+    .where(eq(treasuries.id, treasuryId));
 }
