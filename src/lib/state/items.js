dd6282a338eb4300e1eacd370f67366e9265c70b
Mares1492 <msumarok@tlu.ee>
2025-09-11 22:22:33 +0300

    Add buildingIcons

--- src/lib/state/items.js
+++ src/lib/state/items.js
@@ -1,1 +1,3 @@
 export const itemsIcons = import.meta.glob(["$lib/assets/items/armor/*.png","$lib/assets/items/feet/*.png","$lib/assets/items/hands/*.png","$lib/assets/items/head/*.png","$lib/assets/items/legs/*.png","$lib/assets/items/magic_off_hand/*.png","$lib/assets/items/neck/*.png","$lib/assets/items/off_hand/*.png","$lib/assets/items/two_handed_weapon/*.png","$lib/assets/items/weapon/*.png"], {query: '?url' });
+
+export const buildingIcons =import.meta.glob(["$lib/assets/icons/keep/*.webp"], {query: '?url' });
