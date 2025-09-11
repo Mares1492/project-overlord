797d2dc7a2365d4a3fb05f9c9b8d037b037210ed
Mares1492 <msumarok@tlu.ee>
2025-09-11 20:48:04 +0300

    Create keepBuildings.js

---
+++ src/lib/server/db/seed_data/keepBuildings.js
@@ -0,0 +1,161 @@
+export const keepLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 5000
+    },
+    {
+        id:3,
+        upgradePrice: 10000
+    },
+    {
+        id:4,
+        upgradePrice: 25000
+    },
+    {
+        id:5,
+        upgradePrice: 50000
+    },
+    {
+        id:6,
+        upgradePrice: 100000
+    }
+]
+
+export const barracksLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 2000
+    },
+    {
+        id:3,
+        upgradePrice: 5000
+    },
+    {
+        id:4,
+        upgradePrice: 10000
+    },
+    {
+        id:5,
+        upgradePrice: 15000
+    },
+    {
+        id:6,
+        upgradePrice: 25000
+    }
+]
+
+export const treasuryLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 2000
+    },
+    {
+        id:3,
+        upgradePrice: 5000
+    },
+    {
+        id:4,
+        upgradePrice: 10000
+    },
+    {
+        id:5,
+        upgradePrice: 15000
+    },
+    {
+        id:6,
+        upgradePrice: 25000
+    }
+]
+
+export const academyLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 2000
+    },
+    {
+        id:3,
+        upgradePrice: 5000
+    },
+    {
+        id:4,
+        upgradePrice: 10000
+    },
+    {
+        id:5,
+        upgradePrice: 15000
+    },
+    {
+        id:6,
+        upgradePrice: 25000
+    }
+]
+
+export const arsenalLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 2000
+    },
+    {
+        id:3,
+        upgradePrice: 5000
+    },
+    {
+        id:4,
+        upgradePrice: 10000
+    },
+    {
+        id:5,
+        upgradePrice: 15000
+    },
+    {
+        id:6,
+        upgradePrice: 25000
+    }
+]
+
+export const tombLvlValues = [
+    {
+        id:1,
+        upgradePrice: 1000
+    },
+    {
+        id:2,
+        upgradePrice: 2000
+    },
+    {
+        id:3,
+        upgradePrice: 5000
+    },
+    {
+        id:4,
+        upgradePrice: 10000
+    },
+    {
+        id:5,
+        upgradePrice: 15000
+    },
+    {
+        id:6,
+        upgradePrice: 25000
+    }
+]
