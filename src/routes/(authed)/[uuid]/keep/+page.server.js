37b93e73d976096d0fd02773018482b2138c385e
Mares1492 <msumarok@tlu.ee>
2025-09-12 22:26:59 +0300

    Add upgradeBuilding action

--- src/routes/(authed)/[uuid]/keep/+page.server.js
+++ src/routes/(authed)/[uuid]/keep/+page.server.js
@@ -1,4 +1,6 @@
 import {getKeepData} from '$lib/server/services/keeps'
+import { handleBuildingUpgrade } from '$lib/server/services/keeps'
+import { error } from 'console'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({params}) {
@@ -6,3 +8,16 @@ export async function load({params}) {
     return {keep:KeepData}
 }
 
+/** @satisfies {import('./$types').Actions} */
+export const actions = {
+    upgradeBuilding: async ({ request }) => {
+        const data = await request.formData();
+        const buildingId = data.get('buildingId')
+        const buildingType = data.get('buildingType')
+        if (!buildingId || !buildingType) {
+            console.log("could not read input data")
+            return {error:true,message:"could not read input data"}
+        }
+        await handleBuildingUpgrade(buildingType, buildingId)
+    }
+}
