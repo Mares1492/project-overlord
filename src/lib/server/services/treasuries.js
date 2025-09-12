1366f741e5f9aba2ce62fc3b458c10d90f750c08
Mares1492 <msumarok@tlu.ee>
2025-09-12 22:26:29 +0300

    Create removeFromTreasury

--- src/lib/server/services/treasuries.js
+++ src/lib/server/services/treasuries.js
@@ -14,11 +14,20 @@ export const getTreasuryData = async (userId) => {
     return treasuryData
 }
 
-export const addToTreasury = async (treasuryId,goldToAdd=0,gemsToAdd=0) => {
-    await db.update(treasuries)
+export const addToTreasury = async (treasuryId,goldToAdd=0,gemsToAdd=0,tx=db) => {
+    await tx.update(treasuries)
     .set({
         gold: sql`${treasuries.gold} + ${goldToAdd}`,
         gems: sql`${treasuries.gems} + ${gemsToAdd}`,
+    })
+    .where(eq(treasuries.id, treasuryId));
+}
+
+export const removeFromTreasury = async (treasuryId,goldToRemove=0,gemsToRemove=0,tx=db) => {
+    await tx.update(treasuries)
+    .set({
+        gold: sql`GREATEST(${treasuries.gold} - ${goldToRemove}, 0)`,
+        gems: sql`GREATEST(${treasuries.gold} - ${gemsToRemove}, 0)`,
     })
     .where(eq(treasuries.id, treasuryId));
 }
